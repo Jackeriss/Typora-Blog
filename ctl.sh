431be8f2b19b18b782c474df921e48908f0ce95b
yufeizhao <yufeizhao@clubfactory.com>
2018-12-08 21:12:25 +0800

    update

---
+++ ctl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function start()
+{
+    echo "start $app_name..."
+    supervisorctl start ${app_name}:
+    sleep 3
+    echo "start done"
+}
+
+function stop()
+{
+    echo "stop $app_name..."
+    supervisorctl stop ${app_name}:
+    sleep 3
+    echo "stop done"
+}
+
+function restart()
+{
+    echo "restart $app_name..."
+    for port in $( seq 8101 8102 )
+    do
+        supervisorctl restart ${app_name}_${port}
+        sleep 0.1
+    done
+    sleep 1
+    echo "restart done"
+}
+
+opt="$1"
+app_name="$2"
+if [ "$opt" == "" ]
+then
+    printf "usage: \n\tstart {app_name}\n\tstop {app_name}\n\trestart {app_name}\n"
+    exit
+else
+    "$1"
+fi
